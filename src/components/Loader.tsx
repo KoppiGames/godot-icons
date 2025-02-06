e29f1be63d794c725aaa2266453d98565ae7845c
Mike <mjcuello17@gmail.com>
2025-02-06 14:28:11 -0300

    Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <h3>Loading...</h3>
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
